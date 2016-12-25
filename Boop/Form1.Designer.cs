cf0e090099aa0513fdc16dd8cb25ce03ff248f01
miltoncandelero <miltoncandelero@gmail.com>
2016-12-25 15:05:09 -0300

    Boop v1.0 release
    Snek is happy

---
+++ Boop/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Boop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.File = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnBoop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt3DS = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnAbout = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAbout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pick files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.File});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 201);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(350, 151);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // File
+            // 
+            this.File.Text = "Files to boop";
+            this.File.Width = 346;
+            // 
+            // btnBoop
+            // 
+            this.btnBoop.Location = new System.Drawing.Point(12, 358);
+            this.btnBoop.Name = "btnBoop";
+            this.btnBoop.Size = new System.Drawing.Size(350, 42);
+            this.btnBoop.TabIndex = 2;
+            this.btnBoop.Text = "BOOP";
+            this.btnBoop.UseVisualStyleBackColor = true;
+            this.btnBoop.Click += new System.EventHandler(this.btnBoop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "3DS IP adress: ";
+            // 
+            // txt3DS
+            // 
+            this.txt3DS.Location = new System.Drawing.Point(99, 174);
+            this.txt3DS.MaxLength = 15;
+            this.txt3DS.Name = "txt3DS";
+            this.txt3DS.Size = new System.Drawing.Size(92, 20);
+            this.txt3DS.TabIndex = 6;
+            this.txt3DS.Text = "192.168.1.1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 415);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(375, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.StatusLabel.Text = "Ready";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(160, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(204, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/miltoncandelero/Boop";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Image = global::Boop.Properties.Resources.Boop;
+            this.btnAbout.Location = new System.Drawing.Point(12, 16);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(350, 150);
+            this.btnAbout.TabIndex = 4;
+            this.btnAbout.TabStop = false;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 437);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txt3DS);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnBoop);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Boop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAbout)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnBoop;
+        private System.Windows.Forms.PictureBox btnAbout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt3DS;
+        private System.Windows.Forms.ColumnHeader File;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Boop/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Boop
+{
+    public partial class Form1 : Form
+    {
+        SimpleHTTPServer myServer;
+        Socket s;
+        String[] FilesToBoop;
+        String ActiveDir;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            try
+            {
+                //Just in case, fixing the firewall again. I honestly want to be good snek now.
+                string arguments = "advfirewall firewall delete rule name=\"BOOPFILESERVER\"";
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("netsh", arguments);
+
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+
+                Process.Start(procStartInfo);
+            }
+            catch {}
+
+            try
+            {
+                myServer.Stop();
+            }
+            catch { }
+
+            try
+            {
+                s.Close();
+            }
+            catch { }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog OFD = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            OFD.Filter = "FBI compatible files (*.cia, *.tik)|*.cia;*.tik";
+            OFD.FilterIndex = 0;
+
+            OFD.Multiselect = true;
+
+            // Call the ShowDialog method to show the dialog box.
+
+
+
+            bool? userClickedOK = (OFD.ShowDialog() == DialogResult.OK);
+
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == true)
+            {
+                if (OFD.FileNames.Length > 0) {
+                    listView1.Items.Clear();
+                    FilesToBoop = OFD.FileNames;
+                    ActiveDir = (Path.GetDirectoryName(FilesToBoop[0]));
+
+                    foreach (string item in OFD.FileNames)
+                    {
+                        if (ActiveDir == Path.GetDirectoryName(item))
+                        {
+                            listView1.Items.Add(Path.GetFileName(item));
+                        }
+                        else
+                        {
+                            MessageBox.Show("Somehow you managed to pick 2 files that are in different folders." + Environment.NewLine + "Multi-File booping would need the entire computer hosted to the network and that doesn't feel safe in my book." + Environment.NewLine + "Maybe in the future I'll find a way to do this.", "Woah there...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        
+                    }
+                }
+
+             }
+        }
+
+        private void btnBoop_Click(object sender, EventArgs e)
+        {
+            if (ValidateIPv4(txt3DS.Text) == false)
+            {
+                MessageBox.Show("That doesn't look like an IP adress." + Environment.NewLine + "An IP adress looks something like this: 192.168.1.6" + Environment.NewLine + "(That is: Numbers DOT numbers DOT numbers DOT numbers)", "Error on the IP adress", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //reenable controls?
+
+                return;
+            }
+
+            //MURDERING YOUR FIREWALL
+            //I AM BAD SNEK. MUAHAHAHAHAHA
+
+            string arguments = "advfirewall firewall add rule name=\"BOOPFILESERVER\" dir=in action=allow protocol=TCP localport=8080";
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("netsh", arguments);
+
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+
+            Process.Start(procStartInfo);
+
+
+
+            //blocking all controls?
+            StatusLabel.Text = "Booping... please wait";
+
+            btnBoop.Enabled = false;
+            button1.Enabled = false;
+            txt3DS.Enabled = false;
+            btnAbout.Enabled = false;
+
+
+            myServer = new SimpleHTTPServer(ActiveDir, 8080);
+            
+
+            System.Threading.Thread.Sleep(100);
+
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect(txt3DS.Text, 5000);
+            String message = "";
+            foreach (var CIA in FilesToBoop)
+            {
+               message += LocalIP() + ":8080/" + Path.GetFileName(CIA) + "\n";
+            }
+
+         
+            //boop the info to the 3ds...
+            byte[] Largo = BitConverter.GetBytes((uint)Encoding.ASCII.GetBytes(message).Length);
+            byte[] Adress = Encoding.ASCII.GetBytes(message);
+
+            Array.Reverse(Largo); //Endian fix
+
+
+            
+            s.Send(AppendTwoByteArrays(Largo, Adress));
+            
+
+            s.BeginReceive(new byte[1], 0,1, 0, new AsyncCallback(GotData), null); //Call me back when the 3ds says something.
+
+        }
+
+        private void GotData(IAsyncResult ar)
+        {
+            
+            // now we unlock the controls...
+            //Spooky "thread safe" way to access UI from ASYNC.
+            this.Invoke((MethodInvoker)delegate
+            {
+                btnBoop.Enabled = true;
+                button1.Enabled = true;
+                txt3DS.Enabled = true;
+                btnAbout.Enabled = true;
+                StatusLabel.Text = "Booping complete!";
+            });
+
+            //Fixing your firewall. I am good snek now
+            string arguments = "advfirewall firewall delete rule name=\"BOOPFILESERVER\"";
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("netsh", arguments);
+
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+
+            Process.Start(procStartInfo);
+
+
+            s.Close();
+            myServer.Stop();
+        }
+
+        static byte[] AppendTwoByteArrays(byte[] arrayA, byte[] arrayB)
+        {
+            byte[] outputBytes = new byte[arrayA.Length + arrayB.Length];
+            Buffer.BlockCopy(arrayA, 0, outputBytes, 0, arrayA.Length);
+            Buffer.BlockCopy(arrayB, 0, outputBytes, arrayA.Length, arrayB.Length);
+            return outputBytes;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public static string LocalIP()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        public bool ValidateIPv4(string ipString)
+        {
+            if (String.IsNullOrWhiteSpace(ipString))
+            {
+                return false;
+            }
+
+            string[] splitValues = ipString.Split('.');
+            if (splitValues.Length != 4)
+            {
+                return false;
+            }
+
+            byte tempForParsing;
+
+            return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Boop" + Environment.NewLine + "Coded by Elemental Code (Milton Candelero)" + Environment.NewLine + Environment.NewLine + "Boop is the C# port of servefiles.py for FBI by Steveice10" + Environment.NewLine + Environment.NewLine + "No sneks were harmed in the coding of this app","BOOP 1.0.Snek",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/miltoncandelero/Boop");
+        }
+    }
+}
