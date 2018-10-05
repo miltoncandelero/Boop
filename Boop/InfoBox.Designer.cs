7dbf19177d778f5607d2d0cefec3814b2c6268e3
miltoncandelero <miltoncandelero@gmail.com>
2018-10-04 21:32:37 -0300

    No more json shenneniygans means a smaller file <3

--- Boop/InfoBox.Designer.cs
+++ Boop/InfoBox.Designer.cs
@@ -27,121 +27,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lvContributors = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblSnekFriendly = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // lvContributors
-            // 
-            this.lvContributors.BackColor = System.Drawing.SystemColors.Control;
-            this.lvContributors.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lvContributors.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1});
-            this.lvContributors.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvContributors.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this.lvContributors.HideSelection = false;
-            this.lvContributors.Location = new System.Drawing.Point(6, 19);
-            this.lvContributors.Name = "lvContributors";
-            this.lvContributors.Size = new System.Drawing.Size(271, 153);
-            this.lvContributors.TabIndex = 3;
-            this.lvContributors.UseCompatibleStateImageBehavior = false;
-            this.lvContributors.View = System.Windows.Forms.View.Details;
-            this.lvContributors.ColumnWidthChanging += new System.Windows.Forms.ColumnWidthChangingEventHandler(this.lvContributors_ColumnWidthChanging);
-            this.lvContributors.SelectedIndexChanged += new System.EventHandler(this.lvContributors_SelectedIndexChanged);
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Shout out to all the friends of the snek:";
-            this.columnHeader1.Width = 264;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.lvContributors);
-            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 141);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(283, 178);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Shout out to all the friends of the snek:";
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(153, 21);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(75, 32);
-            this.lblTitle.TabIndex = 6;
-            this.lblTitle.Text = "Boop";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(224, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 17);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "v1.2.0";
-            // 
-            // lblSnekFriendly
-            // 
-            this.lblSnekFriendly.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSnekFriendly.Location = new System.Drawing.Point(132, 57);
-            this.lblSnekFriendly.Name = "lblSnekFriendly";
-            this.lblSnekFriendly.Size = new System.Drawing.Size(163, 67);
-            this.lblSnekFriendly.TabIndex = 8;
-            this.lblSnekFriendly.Text = "Boop, the snek friendly network file transfer for FBI 3DS.";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Boop.Properties.Resources.snekicon1;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(114, 112);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
-            // InfoBox
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(307, 332);
-            this.Controls.Add(this.lblSnekFriendly);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "InfoBox";
-            this.Padding = new System.Windows.Forms.Padding(9);
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "About Boop 1.2.0";
-            this.Load += new System.EventHandler(this.InfoBox_Load);
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.lblTitle = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblSnekFriendly = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// lblTitle
+			// 
+			this.lblTitle.AutoSize = true;
+			this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitle.Location = new System.Drawing.Point(153, 5);
+			this.lblTitle.Name = "lblTitle";
+			this.lblTitle.Size = new System.Drawing.Size(75, 32);
+			this.lblTitle.TabIndex = 6;
+			this.lblTitle.Text = "Boop";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(224, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 17);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "v1.2.0";
+			// 
+			// lblSnekFriendly
+			// 
+			this.lblSnekFriendly.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSnekFriendly.Location = new System.Drawing.Point(132, 41);
+			this.lblSnekFriendly.Name = "lblSnekFriendly";
+			this.lblSnekFriendly.Size = new System.Drawing.Size(163, 83);
+			this.lblSnekFriendly.TabIndex = 8;
+			this.lblSnekFriendly.Text = "Boop, the snek friendly network file transfer for Tinfoil (Switch) and FBI (3DS)." +
+    "";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Boop.Properties.Resources.snekicon1;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(114, 112);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// InfoBox
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(307, 135);
+			this.Controls.Add(this.lblSnekFriendly);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblTitle);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "InfoBox";
+			this.Padding = new System.Windows.Forms.Padding(9);
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "About Boop 1.2.0";
+			this.Load += new System.EventHandler(this.InfoBox_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ListView lvContributors;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label label1;
--- Boop/InfoBox.cs
+++ Boop/InfoBox.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Drawing;
 using System.IO;
 using System.Net;
@@ -21,66 +20,6 @@ namespace Boop
         {
             this.Text = "Boop " + Utils.GetCurrentVersion();
             label1.Text = Utils.GetCurrentVersion();
-            new Task(LoadContributors).Start();
         }
-
-        private void LoadContributors()
-        {
-            try
-            {
-            HttpWebRequest HttpRequestObj = (HttpWebRequest)HttpWebRequest.Create(@"https://api.github.com/repos/miltoncandelero/Boop/contributors");
-            HttpRequestObj.Credentials = CredentialCache.DefaultCredentials;
-            HttpRequestObj.ContentType = "application/json";
-            HttpRequestObj.Method = "GET";
-            HttpRequestObj.Accept = "application/json";
-            HttpRequestObj.UserAgent = "Boop"; // NEEDS SOMETHING WRITTEN!
-            HttpWebResponse response = (HttpWebResponse)HttpRequestObj.GetResponse();
-            string content = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            
-
-            JArray a = JArray.Parse(content);
-
-
-            this.Invoke((MethodInvoker)delegate
-            {
-                lvContributors.Items.Clear();
-                foreach (JObject o in a.Children<JObject>())
-                {
-                    lvContributors.Items.Add((string)o["login"]);
-                }
-            });
-
-            }
-            catch (Exception)
-            {
-                this.Invoke((MethodInvoker)delegate
-                {
-                    //shrink all
-                    this.Height = 174;
-                });
-            }
-
-        }
-
-        private void lvContributors_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
-        {
-            //Keep the width not changed.
-            e.NewWidth = ((ListView)sender).Columns[e.ColumnIndex].Width;
-            //Cancel the event.
-            e.Cancel = true;
-        }
-
-        private void lvContributors_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //Pls no touching the snek list.
-            lvContributors.SelectedIndices.Clear();
-        }
-
-        private void lvContributors_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
-        {
-            e.Graphics.FillRectangle(Brushes.Red, e.Bounds);
-            e.DrawText();
-        }
-
     }
 }
